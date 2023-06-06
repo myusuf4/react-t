abe21cf841b2f6b2ed830a85be9502d2752c8629
Muhammad <121952755+myusuf4@users.noreply.github.com>
2023-06-06 12:38:27 +0500

    component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Count from './Count'
-import Input from './Input';
-import Select from './select';
+import Count from './components/Count'
+import Input from './components/Input';
+import Select from './components/select';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
